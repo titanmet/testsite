df23bd90cb2d4c3ce8ad35e48b58d07b50f29520
titanmet <titanmet@mail.ru>
2020-08-22 20:48:04 +0300

    Initial commit

---
+++ testsite/news/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import News
+
+
+def index(request):
+    # print(request)
+    # news = News.objects.all()
+    # res = '<h1>Список новостей</h1>'
+    # for item in news:
+    #     res += f'<div>\n<p>{item.title}</p>\n<p>{item.content}</p>\n</div>\n<hr>\n'
+    # return HttpResponse(res)
+    news = News.objects.order_by('-created_at')
+    context = {
+        'news': news,
+        'title': 'Список новостей'
+    }
+    return render(request, template_name='news/index.html', context=context)
+
+# def test(request):
+#     return HttpResponse('<H1>Тестовая страница</H1>')
