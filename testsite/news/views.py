4e2ca5b5f3a4cb3b97871bd541863bb6d39132e3
titanmet <titanmet@mail.ru>
2020-08-22 22:24:12 +0300

    adminka

--- testsite/news/views.py
+++ testsite/news/views.py
@@ -10,7 +10,7 @@ def index(request):
     # for item in news:
     #     res += f'<div>\n<p>{item.title}</p>\n<p>{item.content}</p>\n</div>\n<hr>\n'
     # return HttpResponse(res)
-    news = News.objects.order_by('-created_at')
+    news = News.objects.all()
     context = {
         'news': news,
         'title': 'Список новостей'
