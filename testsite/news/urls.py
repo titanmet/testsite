c2e75bf62830c6d6b18182515a7e4a035decbe91
titanmet <titanmet@mail.ru>
2020-08-25 15:50:07 +0300

    url

--- testsite/news/urls.py
+++ testsite/news/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='home'),
     # path('test/', test),
-    path('category/<int:category_id>/', get_category)
+    path('category/<int:category_id>/', get_category, name='category')
 ]
