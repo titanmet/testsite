d9fa3edee36fe9d67b4a29e885838bac1a1e4d1e
titanmet <titanmet@mail.ru>
2020-08-25 12:24:32 +0300

    category

--- testsite/news/urls.py
+++ testsite/news/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('', index),
     # path('test/', test),
+    path('category/<int:category_id>/', get_category)
 ]
--- testsite/news/views.py
+++ testsite/news/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import News
+from .models import News, Category
 
 
 def index(request):
@@ -11,11 +11,25 @@ def index(request):
     #     res += f'<div>\n<p>{item.title}</p>\n<p>{item.content}</p>\n</div>\n<hr>\n'
     # return HttpResponse(res)
     news = News.objects.all()
+    categories = Category.objects.all()
     context = {
         'news': news,
-        'title': 'Список новостей'
+        'title': 'Список новостей',
+        'categories': categories,
     }
     return render(request, template_name='news/index.html', context=context)
 
+
 # def test(request):
 #     return HttpResponse('<H1>Тестовая страница</H1>')
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    context = {
+        'news': news,
+        'categories': categories,
+        'category': category,
+    }
+    return render(request, template_name='news/category.html', context=context)
