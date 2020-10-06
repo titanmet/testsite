8d6a648c464e506e0006b16ef7cc0e678cb660bb
titanmet <titanmet@mail.ru>
2020-10-06 22:34:53 +0300

    register 1

--- testsite/news/urls.py
+++ testsite/news/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('register/', register, name='register'),
+    path('login/', login, name='login'),
     path('test/', test, name='test'),
     # path('', index, name='home'),
     path('', HomeNews.as_view(), name='home'),
