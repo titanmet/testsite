b7923289dce2fe53ca7724cdfd574b8db20ce6b0
titanmet <titanmet@mail.ru>
2020-10-11 21:31:08 +0300

    cache

--- testsite/news/templatetags/news_tags.py
+++ testsite/news/templatetags/news_tags.py
@@ -2,6 +2,7 @@ from django import template
 from django.db.models import Count, F
 
 from news.models import Category
+from django.core.cache import cache
 
 register = template.Library()
 
@@ -15,5 +16,8 @@ def get_categories():
 def show_categories():
     # categories = Category.objects.all()
     # categories = Category.objects.annotate(cnt=Count('news')).filter(cnt__gt=0)
-    categories = Category.objects.annotate(cnt=Count('news', filter=F('news__is_published'))).filter(cnt__gt=0)
+    categories = cache.get('categories')
+    if not categories:
+        categories = Category.objects.annotate(cnt=Count('news', filter=F('news__is_published'))).filter(cnt__gt=0)
+        cache.set('categories', categories, 50)
     return {"categories": categories}
--- testsite/news/urls.py
+++ testsite/news/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import *
+from django.views.decorators.cache import cache_page
 
 urlpatterns = [
     path('register/', register, name='register'),
@@ -7,7 +8,7 @@ urlpatterns = [
     path('logout/', user_logout, name='logout'),
     path('contact/', contact, name='contact'),
     # path('', index, name='home'),
-    path('', HomeNews.as_view(), name='home'),
+    path('', cache_page(60)(HomeNews.as_view()), name='home'),
     # path('category/<int:category_id>/', get_category, name='category'),
     path('category/<int:category_id>/', NewByCategory.as_view(), name='category'),
     # path('news/<int:news_id>/', view_news, name='view_news'),
