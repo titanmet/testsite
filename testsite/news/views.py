d390c403100abc9959738da2c16172e9176e2ae0
titanmet <titanmet@mail.ru>
2020-10-11 20:44:01 +0300

    captcha

--- testsite/news/views.py
+++ testsite/news/views.py
@@ -43,7 +43,7 @@ def user_logout(request):
     return redirect('login')
 
 
-def test(request):
+def contact(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
@@ -51,11 +51,11 @@ def test(request):
                       ['titanmet@mail.ru'], fail_silently=True)
             if mail:
                 messages.success(request, 'Письмо отправлено !')
-                return redirect('test')
+                return redirect('contact')
             else:
                 messages.error(request, 'Ошибка отправки !')
         else:
-            messages.error(request, 'Ошибка регистрации')
+            messages.error(request, 'Ошибка валидации')
     else:
         form = ContactForm()
     return render(request, 'news/test.html', {"form": form})
--- testsite/testsite/settings.py
+++ testsite/testsite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'ckeditor',
     'ckeditor_uploader',
+    'captcha',
     'news.apps.NewsConfig',
 ]
 
@@ -205,3 +206,7 @@ CKEDITOR_CONFIGS = {
         ]),
     }
 }
+
+CAPTCHA_LETTER_ROTATION = False
+CAPTCHA_CHALLENGE_FUNCT = 'captcha.helpers.math_challenge'
+CAPTCHA_NOISE_FUNCTIONS = None
