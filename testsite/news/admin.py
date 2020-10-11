a509f6c0dce2e1b5d6db9af798306bfcc03caf28
titanmet <titanmet@mail.ru>
2020-10-11 19:30:48 +0300

    ckeditor

--- testsite/news/admin.py
+++ testsite/news/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
 
+from django import forms
 from .models import News, Category
 
 
+class NewsAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = News
+        fields = '__all__'
+
+
 class NewsAdmin(admin.ModelAdmin):
+    form = NewsAdminForm
     list_display = ('id', 'title', 'category', 'created_at', 'updated_at', 'is_published', 'get_photo')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
@@ -14,7 +25,6 @@ class NewsAdmin(admin.ModelAdmin):
     readonly_fields = ('get_photo', 'views', 'created_at', 'updated_at')
     save_on_top = True
 
-
     def get_photo(self, obj):
         if obj.photo:
             return mark_safe(f'<img src="{obj.photo.url}" width="75">')
@@ -22,6 +32,7 @@ class NewsAdmin(admin.ModelAdmin):
             return 'Фото нет'
 
     get_photo.short_description = 'Миниатюра'
+
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'title')
@@ -34,4 +45,3 @@ admin.site.register(Category, CategoryAdmin)
 
 admin.site.site_title = 'Управление новостями'
 admin.site.site_header = 'Управление новостями'
-
